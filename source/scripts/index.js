ef6ef8870851722eee1766bae336e4bad98accca
darin-00 <dacota1900@mail.ru>
2024-11-28 21:53:13 +0300

    Module6 task1 (#12)
    * Крупные сетки главной
    
    * Добавляет сетки главной
    
    * проверка
    
    * ошибка
    
    * ошибка
    
    * ошибка
    
    * ошибка
    
    * Добавляет сетки каталога
    
    * Добавляет сетку подписки, цифры, испрвляет блок адрес
    
    * исправляет ошибки
    
    * Добавляет карту и рабочую кнопку
    
    * ошибка
    
    * ошибка
    
    * Добавляет декоры, состояния, карту
    
    * ошибка
    
    * исправление бордер карты
    
    * Окончательная верстка главной
    
    * Выравнивает все, удаляет лишнее
    
    * ошибка

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.navigation');
+const navButton = document.querySelector('.navigation__menu-button');
+
+nav.classList.remove('navigation--nojs');
+
+navButton.addEventListener('click', () => {
+  if (nav.classList.contains('navigation--close')) {
+    nav.classList.remove('navigation--close');
+    nav.classList.add('navigation--open');
+  } else {
+    nav.classList.add('navigation--close');
+    nav.classList.remove('navigation--open');
+  }
+});
