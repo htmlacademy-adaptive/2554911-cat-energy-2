b81547e9e56b5027d72cad4f35e0d7a042af3881
darin-00 <dacota1900@mail.ru>
2024-11-29 13:37:51 +0300

    Module7-task1 (#13)
    * исправляет ошибки фона
    
    * пробую вернуть скрипт
    
    * исправляет ошибки градиента
    
    * Исправляет сдвиги
    
    * исправляет файлы скрипта

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -12,3 +12,32 @@ navButton.addEventListener('click', () => {
     nav.classList.remove('navigation--open');
   }
 });
+
+const slider = document.querySelector('.demo');
+const curtain = slider.querySelector('.demo__curtain');
+const sliderStyles = getComputedStyle(slider);
+let curtainPlaceStart;
+let clientX;
+
+slider.classList.remove('demo--nojs');
+curtain.classList.remove('demo__curtain--nojs');
+
+window.addEventListener('pointerup', stopTheCurtainShifting);
+curtain.addEventListener('pointerdown', startTheCurtainShifting);
+
+function startTheCurtainShifting (event) {
+  curtainPlaceStart = Number(sliderStyles.getPropertyValue('--curtain-place'));
+  clientX = event.clientX;
+  window.addEventListener('pointermove', shiftТheСurtain);
+}
+
+function shiftТheСurtain (event) {
+  const deltaX = event.clientX - clientX;
+  const cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth;
+  const curtainPlace = Math.min(Math.max(cursorPlace, 0), 1);
+  slider.style.setProperty('--curtain-place', `${curtainPlace}`);
+}
+
+function stopTheCurtainShifting () {
+  window.removeEventListener('pointermove', shiftТheСurtain);
+}
